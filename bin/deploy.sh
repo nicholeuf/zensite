0f57711dc007db14f36f8291256c617fdf32c60f
nichole <nichole.frey@doortodoororganics.com>
2017-12-08 12:53:55 -0500

    deploy tweaks

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,14 +2,16 @@
 dockerrepo="nicholefrey/zensite"
 if git diff-index --quiet HEAD --; then
     set -o errexit; # Exit on error
-echo Step 1/3: Archiving previous production image;
+echo Step 1/4: Creating production build;
+    npm run build;
+echo Step 2/4: Archiving previous production image;
     now=$(date +"%m_%d_%Y");
     docker tag $dockerrepo:latest $dockerrepo:$now;
     docker push $dockerrepo:$now;
-echo Step 2/3: Creating new production image;
+echo Step 3/4: Creating new production image;
     npm run build:prod;
     docker push $dockerrepo;
-echo Step 3/3: Creating elastic beanstalk environment;
+echo Step 1/4: Creating elastic beanstalk environment;
 eb $1;
 else
     echo Please commit your changes first.;
