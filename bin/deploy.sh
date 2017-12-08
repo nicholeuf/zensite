3bc34be8131bd7ff70ac56930a2d8dfb2affd8af
nichole <nichole.frey@doortodoororganics.com>
2017-12-08 12:19:02 -0500

    tweak deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,16 +3,14 @@ dockerrepo="nicholefrey/zensite"
 awsapp="zensite"
 if git diff-index --quiet HEAD --; then
     set -o errexit; # Exit on error
-echo Step 1/4: Creating production build;
-    npm run deploy:prod;
-echo Step 2/4: Archiving previous production image;
+echo Step 1/3: Archiving previous production image;
     now=$(date +"%m_%d_%Y");
     docker tag $dockerrepo:latest $dockerrepo:$now;
     docker push $dockerrepo:$now;
-echo Step 3/4: Creating new production image;
+echo Step 2/3: Creating new production image;
     npm run build:prod;
     docker push $dockerrepo;
-echo Step 4/4: Creating elastic beanstalk environment;
+echo Step 3/3: Creating elastic beanstalk environment;
 eb $1 $awsapp;
 else
     echo Please commit your changes first.;
