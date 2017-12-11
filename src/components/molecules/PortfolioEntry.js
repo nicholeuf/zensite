2e9339475c1724817a9f1cb8d4a0ffa81d08bfb8
nichole <nichole.frey@doortodoororganics.com>
2017-12-11 12:00:53 -0500

    Add a portfolio entry about the website; break out the entries into new molecule components

---
+++ src/components/molecules/PortfolioEntry.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PortfolioEntry = ({ title, children }) => {
+  return (
+    <section className="section">
+      <div className="content">
+        <header>
+          <h2 className="subtitle">{title}</h2>
+        </header>
+        {children}
+      </div>
+    </section>
+  );
+}
+
+export default PortfolioEntry;
